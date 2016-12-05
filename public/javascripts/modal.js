3ab663c7deccb5f3637d9eba7608ea132a1f63c2
lromang <luis.roangarci@gmail.com>
2016-12-04 23:43:43 -0600

    quick fix user id

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -182,10 +182,11 @@ function modalEvents(button, modal, page ) {
             modal.find('.modal-title').text('Registrar venta');
             modal.find('#modal_content').html("");
             modal.find('#modal_content').load('/item/list/sale',{ page: page},function(){
-                var select = $('select[id=vselect]').val();
+                var select = $('input[name=id]').data('user_id')
                 //alert("Usuario: "+ select.options[select.selectedIndex].value);
                 $(this).find('.list-group-item').click(function(){
                     if (confirm("¿Está seguro que quiere vender el artículo: " +  $(this).data('item_id'))){
+                        // Selected discount
                         $.post('/carrito/new', {item_id:$(this).data('item_id'), user_id:select}).done(function (data) {
                             alert(data.message);
                             if(data.status=='Ok'){
