f8b1cd3cd1bbe2414560fa389ff2f48a086f7377
Mario Torres <mario291187@gmail.com>
2016-12-07 23:06:46 -0600

    Login

--- routes/index.js
+++ routes/index.js
@@ -134,7 +134,7 @@ router.get('/signout', function(req, res) {
 
 /* GET login page. */
 router.get('/', isNotAuthenticated, function(req, res, next) {
-        res.render('loginTest/index', { title: '', message : req.flash('message') });
+        res.render('login', { title: '', message : req.flash('message') });
 });
 
 router.get('/principal', isAuthenticated, function (req, res) {
