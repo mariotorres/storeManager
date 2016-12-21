e8708fae56d1abe59de82f599c687a6a42a9a012
lromang <luis.roangarci@gmail.com>
2016-12-21 17:23:22 -0600

    style carrito

--- public/javascripts/carrito.js
+++ public/javascripts/carrito.js
@@ -1,35 +1,30 @@
 //Código de los botones del carrito
 $('.btn').click(function () {
     var button = $(this);
-
-    switch ( button.data('action') ){
-        case 'rm_item':
-            //Código para quitar un artículo del carrito
-            if (confirm('¿Seguro que quieres quitar el artículo del carrito?') ) {
-                $.post('/carrito/rem', {
-                    user_id: button.data('user_id'),
-                    item_id: button.data('item_id')
-                }).done(function (data) {
-                    alert(data.message);
-                    if (data.status == 'Ok') {
-                        // Obtener HTML del carrito
-                        location.reload();
-                    }
-                });
+    if (confirm("¿Está seguro que quiere realizar la venta?")){
+        // Selected discount
+        $.post('/carrito/sell', {}).done(function (data) {
+            alert(data.message);
+            if(data.status=='Ok'){
+                //¿?
             }
-            break;
-        case "make_sale":
-            //Confirmar venta
-            if (confirm("¿Está seguro que quiere realizar la venta?")){
-                // Selected discount
-                $.post('/carrito/sell', {}).done(function (data) {
-                    alert(data.message);
-                    if(data.status=='Ok'){
-                        //¿?
-                    }
-                });
-            }
-            break;
+        });
     }
 });
 
+$('#rem').click(function(){
+    var button = $(this);
+    //Código para quitar un artículo del carrito
+    if (confirm('¿Seguro que quieres quitar el artículo del carrito?') ) {
+        $.post('/carrito/rem', {
+            user_id: button.data('user_id'),
+            item_id: button.data('item_id')
+        }).done(function (data) {
+            alert(data.message);
+            if (data.status == 'Ok') {
+                // Obtener HTML del carrito
+                location.reload();
+            }
+        });
+    }
+});
