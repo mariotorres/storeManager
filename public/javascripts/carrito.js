4fae6280aa701524f95f71fcdf28c67631160545
lromang <luis.roangarci@gmail.com>
2016-12-21 18:52:11 -0600

    all working

--- public/javascripts/carrito.js
+++ public/javascripts/carrito.js
@@ -12,7 +12,53 @@ $('.btn').click(function () {
     }
 });
 
+$('.fa').click(function(){
+    var button = $(this);
+   switch(button.data('action')){
+       case 'rem_item':
+           if (confirm('¿Seguro que quieres quitar el artículo: ' +  button.data('item_name') + ' del carrito?') ) {
+               $.post('/carrito/rem', {
+                   user_id: button.data('user_id'),
+                   item_id: button.data('item_id')
+               }).done(function (data) {
+                   alert(data.message);
+                   if (data.status == 'Ok') {
+                       // Obtener HTML del carrito
+                       location.reload();
+                   }
+               });
+           }
+           break;
+       case 'inc_item':
+           $.post('/carrito/inc', {
+               user_id: button.data('user_id'),
+               item_id: button.data('item_id')
+           }).done(function (data) {
+               alert(data.message);
+               if (data.status == 'Ok') {
+                   // Obtener HTML del carrito
+                   location.reload();
+               }
+           });
+           break;
+       case 'dec_item':
+           $.post('/carrito/dec', {
+               user_id: button.data('user_id'),
+               item_id: button.data('item_id')
+           }).done(function (data) {
+               alert(data.message);
+               if (data.status == 'Ok') {
+                   // Obtener HTML del carrito
+                   location.reload();
+               }
+           });
+           break;
+       default:
+   }
 
+});
+
+/*
 $('#inc').click(function(){
     var button = $(this);
     //Código para quitar un artículo del carrito
@@ -59,4 +105,4 @@ $('#rem').click(function(){
         });
     }
 });
-
+*/
