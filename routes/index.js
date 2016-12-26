eda98c9e5573b126558b0f60a8c54adc910cf455
lromang <luis.roangarci@gmail.com>
2016-12-26 15:49:08 -0600

    more sales logic

--- routes/index.js
+++ routes/index.js
@@ -180,9 +180,9 @@ router.get('/carrito', isAuthenticated, function (req, res) {
         return this.batch([
             this.manyOrNone('select * from carrito, articulos, usuarios where carrito.id_articulo = articulos.id and ' +
                 ' carrito.id_usuario = usuarios.id and carrito.unidades_carrito > 0 and usuarios.id = $1 order by articulo',[ req.user.id ]),
-            this.manyOrNone('select sum(precio * unidades_carrito) from carrito, articulos, usuarios where carrito.id_articulo = articulos.id and ' +
+            this.manyOrNone('select round(sum(precio * unidades_carrito * (1 - discount/100)), 2) from carrito, articulos, usuarios where carrito.id_articulo = articulos.id and ' +
                 ' carrito.id_usuario = usuarios.id and carrito.unidades_carrito > 0 and usuarios.id = $1',[ req.user.id ]),
-            this.manyOrNone('select precio*unidades_carrito as totales from carrito, articulos, usuarios where carrito.id_articulo = articulos.id and ' +
+            this.manyOrNone('select round(precio*unidades_carrito*(1 - discount/100), 2) as totales from carrito, articulos, usuarios where carrito.id_articulo = articulos.id and ' +
                 'carrito.id_usuario = usuarios.id and carrito.unidades_carrito > 0 and usuarios.id = $1 order by articulo',[ req.user.id ])
         ]);
 
