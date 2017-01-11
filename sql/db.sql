31fad492223952b33ab891995fcbc1601441ebae
lromang <luis.roangarci@gmail.com>
2017-01-11 13:21:34 -0600

    credito

--- sql/db.sql
+++ sql/db.sql
@@ -151,6 +151,7 @@ create table ventas (
     hora_venta time,
     monto_pagado_efectivo numeric,
     monto_pagado_tarjeta  numeric,
+    tarjeta_credito  boolean,
     saldo_pendiente numeric,
     id_terminal integer references terminales(id),
     estatus text
