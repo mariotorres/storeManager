f1708916cd4885cef11eea0798ecc12734f5c7c9
Mario Torres <mario291187@gmail.com>
2017-01-13 18:39:31 -0600

    ...

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -5,6 +5,25 @@ $(document).ready(function() {
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
+
+        events: [
+            {
+                title  : 'event1',
+                start  : '2017-01-01'
+            },
+            {
+                title  : 'event2',
+                start  : '2017-01-05',
+                end    : '2017-01-07'
+            },
+            {
+                title  : 'event3',
+                start  : '2017-01-09T12:30:00',
+                allDay : false // will make the time show
+            }
+        ]
+
     })
 
 });
+
