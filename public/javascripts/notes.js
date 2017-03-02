bf29d4fbb6368d9a526ff7e6f1f50b8ddc451d1d
lromang <luis.roangarci@gmail.com>
2017-03-02 16:26:03 -0600

    partial payment

--- public/javascripts/notes.js
+++ public/javascripts/notes.js
@@ -1,6 +1,6 @@
 // Guardar monto en botno
 $('input[name=abono]').change(function(){
-    var monto_pagar = document.getElementById('monto_pagar').textContent;
+    var monto_pagar = $(this).data('monto_pagar');
     if($(this).val() > monto_pagar){
         alert("El monto a abonar no puede ser mayor al monto por pagar");
         $(this).val(monto_pagar);
@@ -11,7 +11,7 @@ $('input[name=abono]').change(function(){
 
 // Actualizar montos
 $('button[name=abonar]').click(function(){
-    var monto_pagar = document.getElementById('monto_pagar').textContent;
+    var monto_pagar = $(this).data('monto_pagar');
     var abono       = $(this).val() === null? 0:$(this).val();
     if(abono <= monto_pagar) {
         var id_item  = $(this).data('item_id');
@@ -25,5 +25,7 @@ $('button[name=abonar]').click(function(){
             if (data.status == 'Ok') {
             }
         })
+    }else{
+        alert("El monto a abonar no puede ser mayor al monto por pagar.");
     }
 });
