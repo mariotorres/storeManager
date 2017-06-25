8f0b23f3d92fc27568b20b3c049ad439b44734a2
Mario Torres <mario291187@gmail.com>
2017-06-25 17:34:51 -0500

    UI

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -3,3 +3,15 @@ $('#downloadsales').click(function () {
         window.location.href = '/exportar/ventas.csv';
     }
 });
+
+$('#downloaditems').click(function () {
+    if (confirm('¿Desea exportar el inventario?')){
+        window.location.href = '/exportar/inventario.csv';
+    }
+});
+
+$('#downloadsuppliers').click(function () {
+    if (confirm('¿Desea exportar la lista de proveedores?')){
+        window.location.href = '/exportar/proveedores.csv';
+    }
+});
