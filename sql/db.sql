eda402c18a9dd02b85dd888ef8d79d4cc360f73f
lromang <luis.roangarci@gmail.com>
2017-10-23 12:47:14 -0500

    ...

--- sql/db.sql
+++ sql/db.sql
@@ -242,19 +242,8 @@ create table articulos (
 drop table if exists articulos_solicitados cascade;
 create table articulos_solicitados (
     id serial primary key,
-    id_proveedor integer references proveedores(id),
-    id_tienda integer references tiendas(id),
-    articulo text,
-    descripcion text,
-    id_marca integer references marcas(id) on delete set null,
-    modelo text,
-    talla text,
-    notas text,
-    precio numeric(1000,2),
-    costo numeric(1000,2),
-    codigo_barras numeric(1000,2),
-    nombre_imagen text,
-    n_existencias integer
+    id_articulo integer references articulos(id),
+    n_solicitudes integer
 );
 
 
